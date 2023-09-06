400fadbeaafb6f3dfa78e22f37a3bca2aa597e1c
Ramón Úbeda <121021497+ramonubeda@users.noreply.github.com>
2023-09-06 20:33:02 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+/* Navbar*/ 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+/* Transitions */
+
+window.sr = ScrollReveal();
+sr.reveal('.bottom-transition', {
+    duration: 1500,
+    origin: 'bottom',
+    distance: '-300px'
+});
+
+
+/* CAMBIO DE TÍTULO AL ABANDONAR LA WEB */
+
+let previousTitle = document.title;
+
+window.addEventListener("blur", () => {
+    previousTitle = document.title;
+    document.title = "Coocking Around The World"
+})
+
+window.addEventListener("focus", () => {
+    document.title = previousTitle
+})
+
+const menu = document.getElementById('menu');
+const indicador = document.getElementById('indicador');
+const secciones = document.querySelectorAll('.seccion');
+
+let tamañoIndicador = menu.querySelector('a').offsetWidth;
+indicador.style.width = tamañoIndicador + 'px';
+
+let indexSeccionActiva;
